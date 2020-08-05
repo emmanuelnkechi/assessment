a05d4f7e342429b9168c1201c4720269c15259c5
Nkechi Emmanuel <nkechiemmanuel95@gmail.com>
2020-08-05 12:16:01 +0100

    answer to the algorithm isum

---
+++ demo.py
@@ -0,0 +1,12 @@
+def isum(a, b):
+    return int(a + b)
+
+isum_result = isum(a, b)
+print(isum(2,3))
+
+
+class DemoClass(self,name):
+    self.name = name
+
+    def getName(self):
+        return f'my name is {}'
